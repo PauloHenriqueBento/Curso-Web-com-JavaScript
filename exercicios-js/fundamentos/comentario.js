2cb861994fdfb1aa65e858b877dd85e50ae10203
Paulo Henrique Bento dos Santos <41974455+PauloHenriqueBento@users.noreply.github.com>
2020-02-04 23:09:41 -0300

    Add files via upload
    Fundamentos de javaScript

---
+++ exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,13 @@
+// Comentário de uma linha
+console.log('Linha 1') //Pode ser colocado na frente
+
+/*
+Comentário de 
+múltiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+* Comentário de Múltiplas linhas
+*/
+console.log('Linha 3')
---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,15 @@
+var a = 3
+let b = 4
+
+/*Var e Let são ambos declaradores de variaveis
+*Porem tentar utilizar mais o Let, é mais moderno
+*/
+
+var a = 30
+b = 40
+
+console.log(a, b);
+
+const c = 5;
+// c = 50
+console.log(c)
